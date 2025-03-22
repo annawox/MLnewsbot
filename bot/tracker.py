8c0ed269ce1c5a6787a2c7de8d9103bfaa0e8ee2
anna <143606587+annawox@users.noreply.github.com>
2025-03-22 12:46:39 +0300

    Update tracker.py

--- bot/tracker.py
+++ bot/tracker.py
@@ -67,7 +67,7 @@ try:
                 message = (
                     f"новый релиз {repo_name}: {latest_release_name}\n"
                     f"[подробнее]({latest_release_url})\n\n"
-                    f"Описание:\n{latest_release_body[:200]}..."  # Обрезаем описание до 200 символов
+                    f"Описание:\n{latest_release_body[:500]}..."  # Обрезаем описание до 500 символов
                 )
                 send_telegram_message(message)
                 last_sent_releases[repo] = latest_release_id  # Обновляем последний отправленный релиз
